864e49f59262fe5d01d481e7b1c6c86c48c292c5
Sujal <122919895+SUJALGPM@users.noreply.github.com>
2023-08-31 20:45:31 +0530

    Add files via upload

---
+++ Factor.R
@@ -0,0 +1,33 @@
+#Factors In R DataStructure................
+
+#Create Factor using factor()
+a<-c("red","green","orange",'violet',"yellow","blue")
+data<-factor(a)
+
+b<-c("Male","Female","Male","Female","Female")
+data1<-factor(b)
+
+#Accessing element from factor...
+data[-1]
+data[5]
+data[1:5]
+
+#Update factors and factors levels...
+data<-factor(a,levels=c("blue","green","red","violet","book","yellow","orange"))
+data[4]<-"black"
+
+levels(data1)<-c("M","F")
+
+#Delete data from factor....
+data[-3]
+data[-2:-4]
+data[-1:-4]
+data[c(-1,-4)]
+
+#Remaining function in Factor...
+typeof(data)
+class(data)
+is.factor(data)
+length(data)
+levels(data)
+
