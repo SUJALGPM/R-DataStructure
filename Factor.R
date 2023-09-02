f268e8d3496b91732f39af6a902a62703783b83a
Sujal <122919895+SUJALGPM@users.noreply.github.com>
2023-09-02 10:45:37 +0530

    Update Factor.R

--- Factor.R
+++ Factor.R
@@ -34,3 +34,24 @@ levels(data)
 #Creating factors using gl() in R
 sum<-gl(4,2,labels=c("sujal","shreeya","Faisal","Harsha"))
 sum
+
+#Create factor having 3 levels manaully.....
+data<-factor(c("male","female","male","female"),levels = c("female","male","transgender"))
+data
+
+#Check the levels is set or not.....
+levels(data)
+
+#Now try to add data in existing levels....
+append(data,"trio",after = 4)
+
+append(data,"transgender",after=4)
+
+#Levels is predefine so you can add value of that only...
+data[5]<-"transgender"
+
+data[6]<-"trio"
+
+data[6]<-"transgender"
+
+data[-7]
